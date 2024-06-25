5a334a32c223f922c3208d9c7dec1f7cca6a7933
bjw <bjw8518@gmail.com>
2024-06-25 18:14:40 +0900

    240625

---
+++ src/test/java/com/bjw/fesedm/FesedmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjw.fesedm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FesedmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
